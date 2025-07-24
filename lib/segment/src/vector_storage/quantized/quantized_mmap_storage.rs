4dc6a394dcb49e4f5d8d6ebd418e7653a8904a98
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2025-07-24 10:22:08 +0200

    appendable qunatization storage
    fmt
    
    deprecate count in quantization config
    
    fix arm tests build
    
    qunatized storage vectors count
    
    fix ci
    
    are you happy clippy
    
    fix compat tests
    
    undo rename to `deprecated_count`
    
    remove flusher
    
    remove obsolete functions, don't use hardware counter in ram storage
    
    are you happy clippy
    
    fix gpu build
    
    check ci when revert option
    
    revert last commit
    
    debug ci
    
    log offsets
    
    log offsets
    
    fix compatibility tests
    
    deprecate count
    
    chunked mmap as a quantization storage
    
    get chunked mmap files list
    
    fix after rebase
    
    fix arm tests
    
    remove quantized storage save
    
    remove quantization save
    
    remove load from quantization trait
    
    refactor load
    
    remove from file
    
    move quantized ram storage
    
    remove multivector offset save
    
    remove multivector offset load
    
    remove chunked mmap impl to reduce pr
    
    restore try_set capacity exact
    
    review remarks
    
    review remarks
    
    review remarks
    
    review remarks
    
    fix build
    
    fmt
    
    fix tests
    
    fmt
    
    rename builder
    
    fix build after rebase

--- lib/segment/src/vector_storage/quantized/quantized_mmap_storage.rs
+++ lib/segment/src/vector_storage/quantized/quantized_mmap_storage.rs
@@ -114,6 +114,18 @@ impl quantization::EncodedStorageBuilder for QuantizedMmapStorageBuilder {
     }
 
     fn push_vector_data(&mut self, other: &[u8]) {
+        debug_assert_eq!(
+            self.quantized_vector_size,
+            other.len(),
+            "Pushed vector size does not match expected quantized vector size"
+        );
+        debug_assert!(
+            self.cursor_pos + other.len() <= self.mmap.len(),
+            "Overflow allocated quantization storage mmap file (cursor_pos {} + len {} > total {})",
+            self.cursor_pos,
+            other.len(),
+            self.mmap.len()
+        );
         self.mmap[self.cursor_pos..self.cursor_pos + other.len()].copy_from_slice(other);
         self.cursor_pos += other.len();
     }
